b67b604a3e33954feaf879e42e7b07fc780dc32d
ignacioedlp <ignaciocafiero12@gmail.com>
2022-06-19 01:49:28 -0300

    authentication persist

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,6 +1,87 @@
 import React from "react";
+import {
+  doCreateUserWithEmailAndPassword,
+  doSendEmailVerification,
+} from "../firebase/firebaseAuth";
+
+import { useRouter } from "next/router";
 
 function RegisterForm() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordConfirm, setPasswordConfirm] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [codePostal, setCodePostal] = React.useState("");
+  const router = useRouter();
+
+  const performEmailSignUp = async (email, password, firstName, lastName) => {
+    try {
+      console.log("Register..");
+      const userCredential = await doCreateUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log("Register Success");
+      console.log(userCredential);
+      await doSendEmailVerification(userCredential.user);
+
+      router.push("/login");
+    } catch {
+      console.log("error");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      email != "" &&
+      password != "" &&
+      passwordConfirm != "" &&
+      firstName != "" &&
+      lastName != "" &&
+      phone != "" &&
+      codePostal != ""
+    ) {
+      if (passwordConfirm === password) {
+        await performEmailSignUp(email, password, firstName, lastName);
+      } else {
+        console.log("Password not match");
+      }
+    } else {
+      console.log("Please fill all fields");
+    }
+  };
+
+  const handleChangeFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleChangePasswordConfirm = (e) => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  const handleChangePhone = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleChangeCodePostal = (e) => {
+    setCodePostal(e.target.value);
+  };
+
   return (
     <header className="dark:bg-gray-800  ">
       <div className="container px-6 py-16 mx-auto">
@@ -31,6 +112,8 @@ function RegisterForm() {
                       </label>
                       <input
                         id="firstName"
+                        value={firstName}
+                        onChange={handleChangeFirstName}
                         type="text"
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
@@ -46,6 +129,8 @@ function RegisterForm() {
                       <input
                         id="lastName"
                         type="text"
+                        value={lastName}
+                        onChange={handleChangeLastName}
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -60,6 +145,8 @@ function RegisterForm() {
                       <input
                         id="postalCode"
                         type="text"
+                        value={codePostal}
+                        onChange={handleChangeCodePostal}
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -74,6 +161,8 @@ function RegisterForm() {
                       <input
                         id="phone"
                         type="number"
+                        value={phone}
+                        onChange={handleChangePhone}
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -88,6 +177,8 @@ function RegisterForm() {
                     <input
                       id="email"
                       type="email"
+                      value={email}
+                      onChange={handleChangeEmail}
                       className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                     />
                   </div>
@@ -95,13 +186,15 @@ function RegisterForm() {
                     <div>
                       <label
                         className="text-body1 font-avenir  dark:text-gray-200"
-                        htmlFor="firstName"
+                        htmlFor="password"
                       >
                         Password
                       </label>
                       <input
-                        id="firstName"
+                        id="password"
                         type="password"
+                        value={password}
+                        onChange={handleChangePassword}
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
@@ -110,18 +203,23 @@ function RegisterForm() {
                     <div>
                       <label
                         className="text-body1 font-avenir  dark:text-gray-200"
-                        htmlFor="firstName"
+                        htmlFor="passwordConfirm"
                       >
                         Confirm Password
                       </label>
                       <input
-                        id="firstName"
+                        id="passwordConfirm"
                         type="password"
+                        value={passwordConfirm}
+                        onChange={handleChangePasswordConfirm}
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-body1 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                       />
                     </div>
                     <div className="flex justify-end mt-6">
-                      <button className="px-6 py-2 leading-5 text-background transition-colors duration-200 transform bg-icon rounded-md hover:bg-heading/80 focus:outline-none  font-avenir focus:bg-gray-600">
+                      <button
+                        onClick={handleSubmit}
+                        className="px-6 py-2 leading-5 text-background transition-colors duration-200 transform bg-icon rounded-md hover:bg-heading/80 focus:outline-none  font-avenir focus:bg-gray-600"
+                      >
                         Register now
                       </button>
                     </div>
